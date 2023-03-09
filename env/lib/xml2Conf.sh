0c17ebd7edb8c6a2dbc63f6150e50950402de1ae
Bhavya Giri <102273412+bhavya-giri@users.noreply.github.com>
2023-03-09 10:51:06 +0530

    first

---
+++ env/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/Users/bhavyagiri/Developer/hands-on-machine-learning/practice/env/lib"
+XML2_LIBS="-lxml2 -L/Users/bhavyagiri/Developer/hands-on-machine-learning/practice/env/lib -lz -L/Users/bhavyagiri/Developer/hands-on-machine-learning/practice/env/lib -llzma -lpthread -L/Users/bhavyagiri/Developer/hands-on-machine-learning/practice/env/lib -liconv -L/Users/bhavyagiri/Developer/hands-on-machine-learning/practice/env/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/Users/bhavyagiri/Developer/hands-on-machine-learning/practice/env/include/libxml2 -I/Users/bhavyagiri/Developer/hands-on-machine-learning/practice/env/include"
+MODULE_VERSION="xml2-2.9.14"
+
---
+++ env/lib/xsltConf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/Users/bhavyagiri/Developer/hands-on-machine-learning/practice/env/lib"
+XSLT_LIBS="-lxslt -L/Users/bhavyagiri/Developer/hands-on-machine-learning/practice/env/lib -lxml2 -L/Users/bhavyagiri/Developer/hands-on-machine-learning/practice/env/lib -L/Users/bhavyagiri/Developer/hands-on-machine-learning/practice/env/lib -lpthread -L/Users/bhavyagiri/Developer/hands-on-machine-learning/practice/env/lib -liconv -L/Users/bhavyagiri/Developer/hands-on-machine-learning/practice/env/lib -licui18n -licuuc -licudata -lm "
+XSLT_PRIVATE_LIBS=""
+XSLT_INCLUDEDIR="-I/Users/bhavyagiri/Developer/hands-on-machine-learning/practice/env/include"
+MODULE_VERSION="xslt-1.1.35"
